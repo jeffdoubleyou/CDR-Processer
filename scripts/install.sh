0621ae68a2eaaa135859a65701c37e463c47ac43
Jeffrey Weitz <jeffdoubleyou@gmail.com>
2021-01-19 11:33:33 -0500

    Some installation changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,8 +25,7 @@ DB_HOST=${DB_HOST:-127.0.0.1}
 read -p "MySQL user: [root]: " DB_USER
 DB_USER=${DB_USER:-root}
 
-read -s -p "MySQL password: [root]: " DB_PASS
-DB_PASS=${DB_PASS:-root}
+read -s -p "MySQL password: [empty]: " DB_PASS
 echo ""
 
 read -p "MySQL database: [cdr]: " DB_DATABASE
@@ -55,19 +54,32 @@ shini_write "$InstallLoc/cargo.conf" "required" "FileDir" "$FileDir"
 shini_write "$InstallLoc/cargo.conf" "required" "DSN" "$DSN"
 
 if [ $OS = "Linux" ]; then
+    echo "Installing Linux binary to $InstallLoc"
     cp bin/cargo_linux $InstallLoc/cargo
     chmod 755 $InstallLoc/cargo
-    if [ -e /sbin/init ]; then
-        if [ $(/sbin/init --version | grep -c upstart) != 0 ]; then
-            echo -n "Install service..."
-            cp scripts/init /etc/init/cargo.conf
-            initctl reload-configuration
-            initctl start cargo
-        fi
+
+    SYSTEMCTL=$(which systemctl)
+    INITCTL=$(which initctl)
+
+    if [ ! -z "$INITCTL" ]; then
+        echo "Installing upstart service"
+        cp scripts/init /etc/init/cargo.conf
+        initctl reload-configuration
+        initctl start cargo
+    elif [ ! -z "$SYSTEMCTL" ]; then
+        echo "Installing systemd service"
+        cp scripts/systemd /etc/systemd/system/cargo.service
+        systemctl daemon-reload
+        systemctl start cargo
     else
-        cp bin/cargo_osx $InstallLoc/cargo
-        chmod 755 $InstallLoc/cargo
+        echo "No sys-v support implemented, please manually run the daemon from the $InstallLoc directory"
     fi
+elif [ $OS = "Darwin" ]; then
+    echo "Installing OSX binary to $InstallLoc"
+    cp bin/cargo_osx $InstallLoc/cargo
+    chmod 755 $InstallLoc/cargo
+else
+    echo "Unknown operating system $OS - please build from source and install manually"
 fi
    
 echo "Cargo installed in $InstallLoc - Edit cargo.conf for additional settings"
